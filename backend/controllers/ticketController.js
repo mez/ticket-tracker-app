9831286f8c250fab774acdc7e830a29d13bebba1
Mez Gebre <mez@jetcode.io>
2022-03-17 14:54:43 -0700

    full CRUD API for ticket model

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,17 +8,114 @@ import Ticket from '../models/ticketModel.js'
 // @access      Private
 export const getTickets = asyncHandler(async (req, res) => {
 
-  res.status(200).json({
-    message: 'get tickets'
-  })
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const tickets = await Ticket.find({user: req.user.id})
+
+  res.status(200).json(tickets)
+})
+
+// @description Get user ticket
+// @route       GET /api/tickets/:id
+// @access      Private
+export const getTicket = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findOne({user: req.user.id, id: req.params.id})
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  res.status(200).json(ticket)
+})
+
+// @description update user ticket
+// @route       PUT /api/tickets/:id
+// @access      Private
+export const updateTicket = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findOne({user: req.user.id, id: req.params.id})
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+  res.status(200).json(updatedTicket)
+})
+
+
+// @description delete a user tickets
+// @route       DELETE /api/tickets/:id
+// @access      Private
+export const deleteTicket = asyncHandler(async (req, res) => {
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findOne({user: req.user.id, id: req.params.id})
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({success: true})
 })
 
 // @description Create new ticket
 // @route       POST /api/tickets
 // @access      Private
 export const createTicket = asyncHandler(async (req, res) => {
+  const {product, description} = req.body
 
-  res.status(200).json({
-    message: 'create ticket'
+  if (!product || !description) {
+    res.status(400)
+    throw new Error('You need to add a product and have a description')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+
+  //creat the ticket
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id
   })
+
+  res.status(201).json(ticket)
 })
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import { getTickets, createTicket } from '../controllers/ticketController.js'
+import { getTickets, createTicket, getTicket, deleteTicket, updateTicket } from '../controllers/ticketController.js'
 
 const router = express.Router()
 
 import protect from '../middleware/authMiddleware.js'
 
-router.route('/').get(protect, getTickets).post(protect, createTicket)
+router.route('/')
+  .get(protect, getTickets)
+  .post(protect, createTicket)
+
+router.route('/:id')
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket)
 
 
 export default router
